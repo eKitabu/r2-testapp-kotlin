74e503246c226edaf0a970ccf6955b2ed7dcabcf
Aferdita Muriqi <aferdita.muriqi@gmail.com>
2018-08-06 19:43:48 -0400

    updated lcp workflow

--- r2-testapp/src/main/java/org/readium/r2/testapp/CatalogActivityPlusLcp.kt
+++ r2-testapp/src/main/java/org/readium/r2/testapp/CatalogActivityPlusLcp.kt
@@ -10,6 +10,7 @@
 package org.readium.r2.testapp
 
 import android.app.ProgressDialog
+import android.content.Context
 import android.net.Uri
 import android.os.Bundle
 import android.widget.EditText
@@ -20,6 +21,7 @@ import nl.komponents.kovenant.then
 import nl.komponents.kovenant.ui.successUi
 import org.jetbrains.anko.*
 import org.jetbrains.anko.appcompat.v7.Appcompat
+import org.jetbrains.anko.design.longSnackbar
 import org.jetbrains.anko.design.textInputLayout
 import org.joda.time.DateTime
 import org.joda.time.format.DateTimeFormat
@@ -27,7 +29,7 @@ import org.readium.r2.lcp.LcpHttpService
 import org.readium.r2.lcp.LcpLicense
 import org.readium.r2.lcp.LcpSession
 import org.readium.r2.shared.Publication
-import org.readium.r2.shared.drm.DRMMModel
+import org.readium.r2.shared.drm.DRMModel
 import org.readium.r2.shared.drm.Drm
 import org.readium.r2.streamer.parser.EpubParser
 import org.readium.r2.streamer.parser.PubBox
@@ -42,48 +44,59 @@ class CatalogActivityPlusLcp : CatalogActivity(), LcpFunctions {
         listener = this
     }
 
+//    Import a DRM license ( needs to be online )
+//
+//    An app which imports a DRM license will follow these steps (see the previous section for more details):
+//
+//    1/ Validate the license structure and check its profile identifier
+//
+//    2/ Get the passphrase associated with the license
+//
+//    3/ Validate the license integrity
+//
+//    4/ Check the license status
+//
+//    5/ Get an updated license if needed
+//
+//    6/ Fetch the encrypted publication
+//
+//    7/ Register the device / license
+//
+//    8/ Open the publication
+
+
+//    Open a protected publication stored in the app catalog ( can work offline as well )
+//
+//    The process is a simpler than when the protected publication is imported, as some information about the license is stored in the database, especially the license identifier.
+//
+//    4/ Check the license status
+//
+//    5/ Get an updated license if needed
+//
+//    8/ Open the publication
+
+
     override fun parseIntentLcpl(uriString: String) {
         val uri: Uri? = Uri.parse(uriString)
         if (uri != null) {
-            val progress = indeterminateProgressDialog(getString(R.string.progress_wait_while_downloading_book))
-            progress.show()
-            val thread = Thread(Runnable {
-                val lcpLicense = LcpLicense(URL(uri.toString()).openStream().readBytes(), this)
-                task {
-                    lcpLicense.fetchStatusDocument().get()
-                } then {
-                    lcpLicense.checkStatus()
-                    lcpLicense.updateLicenseDocument().get()
-                } then {
-                    lcpLicense.areRightsValid()
-                    lcpLicense.register()
-                    lcpLicense.fetchPublication()
-                } then {
-                    it?.let {
-                        lcpLicense.moveLicense(it, URL(uri.toString()).openStream().readBytes())
-                    }
-                    it!!
-                } successUi { path ->
-                    val file = File(path)
+            try {
+                val progress = indeterminateProgressDialog(getString(R.string.progress_wait_while_downloading_book))
+                progress.show()
+                Thread({
                     try {
-                        runOnUiThread({
-                            val parser = EpubParser()
-                            val pub = parser.parse(path)
-                            if (pub != null) {
-                                val pair = parser.parseRemainingResource(pub.container, pub.publication, pub.container.drm)
-                                pub.container = pair.first
-                                pub.publication = pair.second
-                                prepareToServe(pub, file.name, file.absolutePath, true, true)
-                                progress.dismiss()
-
-                            }
-                        })
-                    } catch (e: Throwable) {
-                        e.printStackTrace()
+                        val bytes = URL(uri.toString()).openStream().readBytes()
+                        lcpThread(bytes, progress)
+                    } catch (e: Exception) {
+                        e.localizedMessage?.let {
+                            longSnackbar(catalogView, it)
+                        } ?: run {
+                            longSnackbar(catalogView, "An error occurred")
+                        }
+                        progress.dismiss()
                     }
-                }
-            })
-            thread.start()
+                }).start()
+            } catch (e: Exception) {
+            }
         }
     }
 
@@ -91,7 +104,7 @@ class CatalogActivityPlusLcp : CatalogActivity(), LcpFunctions {
         if (drm.brand == Drm.Brand.Lcp) {
             prepareToServe(pub, book.fileName, file.absolutePath, false, true)
 
-            handleLcpPublication(publicationPath, drm, {
+            handleLcpPassphrase(publicationPath, drm, {
                 val pair = parser.parseRemainingResource(pub.container, publication, it)
                 pub.container = pair.first
                 pub.publication = pair.second
@@ -100,7 +113,7 @@ class CatalogActivityPlusLcp : CatalogActivity(), LcpFunctions {
                     server.addEpub(publication, pub.container, "/" + book.fileName, applicationContext.getExternalFilesDir(null).path + "/styles/UserProperties.json")
 
                     val license = (drm.license as LcpLicense)
-                    val drmModel = DRMMModel(drm.brand.name,
+                    val drmModel = DRMModel(drm.brand.name,
                             license.currentStatus(),
                             license.provider().toString(),
                             DateTime(license.issued()).toString(DateTimeFormat.shortDateTime()),
@@ -124,55 +137,90 @@ class CatalogActivityPlusLcp : CatalogActivity(), LcpFunctions {
     }
 
     override fun processLcpActivityResult(uri: Uri, it: Uri, progress: ProgressDialog) {
-        val input = contentResolver.openInputStream(uri)
-
-        val thread = Thread(Runnable {
-            val lcpLicense = LcpLicense(input.readBytes(), this)
-            task {
-                lcpLicense.fetchStatusDocument().get()
-            } then {
-                lcpLicense.checkStatus()
-                lcpLicense.updateLicenseDocument().get()
-            } then {
-                lcpLicense.areRightsValid()
-                lcpLicense.register()
-                lcpLicense.fetchPublication()
-            } then {
-                it?.let {
-                    lcpLicense.moveLicense(it, contentResolver.openInputStream(uri).readBytes())
-                }
-                it!!
-            } successUi { path ->
-                val file = File(path)
-                try {
-                    runOnUiThread({
-                        val parser = EpubParser()
-                        val pub = parser.parse(path)
-                        if (pub != null) {
-                            val pair = parser.parseRemainingResource(pub.container, pub.publication, pub.container.drm)
-                            pub.container = pair.first
-                            pub.publication = pair.second
-                            prepareToServe(pub, file.name, file.absolutePath, true, true)
-                            progress.dismiss()
-                        }
-                    })
-                } catch (e: Throwable) {
-                    e.printStackTrace()
-                }
-            }
-        })
-        thread.start()
+        Thread({
+            val bytes = contentResolver.openInputStream(uri).readBytes()
+            lcpThread(bytes, progress)
+        }).start()
     }
 
-    fun handleLcpPublication(publicationPath: String, drm: Drm, parsingCallback: (drm: Drm) -> Unit, callback: (drm: Drm) -> Unit, callbackUI: () -> Unit): Promise<Unit, Exception> {
+    private fun lcpThread(bytes: ByteArray, progress: ProgressDialog) {
+        val lcpLicense = LcpLicense(bytes, this)
+        task {
+            lcpLicense.fetchStatusDocument().get()
+        } then {
+            try {
+                lcpLicense.checkStatus()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+            lcpLicense.updateLicenseDocument().get()
+        } then {
+            try {
+                lcpLicense.areRightsValid()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+            lcpLicense.register()
+            lcpLicense.fetchPublication()
+        } then {
+            it?.let {
+                lcpLicense.moveLicense(it, bytes)
+            }
+            it!!
+        } successUi { path ->
+            val file = File(path)
+            runOnUiThread {
+                val parser = EpubParser()
+                val pub = parser.parse(path)
+                if (pub != null) {
+                    val pair = parser.parseRemainingResource(pub.container, pub.publication, pub.container.drm)
+                    pub.container = pair.first
+                    pub.publication = pair.second
+                    prepareToServe(pub, file.name, file.absolutePath, true, true)
+                    progress.dismiss()
+                    handleLcpPassphrase(file.absolutePath, Drm(Drm.Brand.Lcp), {
+                        // Do nothing
+                    }, {
+                        // Do nothing
+                    }, {
+                        // Do nothing
+                    }).get()
+                }
+            }
+        } fail {
+            it.printStackTrace()
+            it.localizedMessage?.let {
+                longSnackbar(catalogView, it)
+            } ?: run {
+                longSnackbar(catalogView, "An error occurred")
+            }
+            progress.dismiss()
+        }
+    }
+
+    fun handleLcpPassphrase(publicationPath: String, drm: Drm, parsingCallback: (drm: Drm) -> Unit, callback: (drm: Drm) -> Unit, callbackUI: () -> Unit): Promise<Unit, Exception> {
         val lcpHttpService = LcpHttpService()
         val session = LcpSession(publicationPath, this)
 
         fun validatePassphrase(passphraseHash: String): Promise<LcpLicense, Exception> {
+
+            val preferences = getSharedPreferences("org.readium.r2.lcp", Context.MODE_PRIVATE)
+
             return task {
-                lcpHttpService.certificateRevocationList("http://crl.edrlab.telesec.de/rl/EDRLab_CA.crl").get()
+                try {
+                    lcpHttpService.certificateRevocationList("http://crl.edrlab.telesec.de/rl/EDRLab_CA.crl").get()
+                } catch (e: Exception) {
+                    null
+                }
             } then { pemCrtl ->
-                session.resolve(passphraseHash, pemCrtl).get()
+                if (pemCrtl != null) {
+                    preferences.edit().putString("pemCrtl", pemCrtl).apply()
+                    session.resolve(passphraseHash, pemCrtl).get()
+                } else {
+                    session.resolve(passphraseHash, preferences.getString("pemCrtl", "")).get()
+                }
+            } fail {
+                it.printStackTrace()
             }
         }
 
@@ -197,6 +245,8 @@ class CatalogActivityPlusLcp : CatalogActivity(), LcpFunctions {
                         } then { validPassphraseHash ->
                             session.storePassphrase(validPassphraseHash)
                             callback(validPassphraseHash)
+                        } fail {
+                            it.printStackTrace()
                         }
                     }
                     negativeButton("Cancel") { }
